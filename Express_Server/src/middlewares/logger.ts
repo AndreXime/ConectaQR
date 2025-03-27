29cb95205b984bc3148b4f8e0735d03a3b311b34
AndreXime <andreximenesa20@gmail.com>
2025-03-27 17:48:02 -0300

    UPDATE: CD Workflow

--- Express_Server/src/middlewares/logger.ts
+++ Express_Server/src/middlewares/logger.ts
@@ -1,32 +1,33 @@
-import type { Request, Response, NextFunction } from "express";
-import fs from "fs";
-import path from "path";
+import type { Request, Response, NextFunction } from 'express';
+import fs from 'fs';
+import path from 'path';
 
-const logFilePath = path.resolve("generated/requests.log");
+const logFilePath = path.resolve('generated/requests.log');
 
 const logger = (req: Request, res: Response, next: NextFunction) => {
-  const start = process.hrtime();
+	const start = process.hrtime();
 
-  res.on("close", () => {
-    if (req.originalUrl === "/favicon.ico") return; // Ignorar favicon
-    const [seconds, nanoseconds] = process.hrtime(start);
-    const segundosPassados = (seconds + nanoseconds / 1e9).toFixed(3);
+	/* res.on close prende essa função até response disparar close que quando a conexao é fechada */
+	res.on('close', () => {
+		if (req.originalUrl === '/favicon.ico') return; // Ignorar favicon
+		const [seconds, nanoseconds] = process.hrtime(start);
+		const segundosPassados = (seconds + nanoseconds / 1e9).toFixed(3);
 
-    const status = res.statusCode;
-    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+		const status = res.statusCode;
+		const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
 
-    const todayDate = new Date().toLocaleString("pt-BR", { timeZone: "America/Sao_Paulo" }).replace(",", "");
+		const todayDate = new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' }).replace(',', '');
 
-    const message = `| ${req.method} ${req.originalUrl} | IP: ${ip} | Tempo: ${segundosPassados} seg | Status: ${status} | Data: ${todayDate} |`;
-    if (process.env.NODE_ENV !== "production") {
-      console.log(message);
-    }
-    fs.appendFile(logFilePath, message + "\n", (err) => {
-      if (err) console.log(err);
-    });
-  });
+		const message = `| ${req.method} ${req.originalUrl} | IP: ${ip} | Tempo: ${segundosPassados} seg | Status: ${status} | Data: ${todayDate} |`;
+		if (process.env.NODE_ENV !== 'production') {
+			console.log(message);
+		}
+		fs.appendFile(logFilePath, message + '\n', (err) => {
+			if (err) console.log(err);
+		});
+	});
 
-  next();
+	next();
 };
 
 export default logger;
